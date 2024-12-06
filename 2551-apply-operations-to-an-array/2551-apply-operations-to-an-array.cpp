ab779fd53f848d2d5060fca379f1c5b73bf7369c
PRADEEPRAJ <139350539+PRADEEPGITtest@users.noreply.github.com>
2024-12-06 22:56:33 +0530

    Time: 0 ms (100%), Space: 13.3 MB (7.84%) - LeetHub

---
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i-1]==nums[i])
+            {
+                nums[i-1]=nums[i-1]*2;
+                nums[i]=0;
+            }
+        }
+        // int ind=0;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     if(nums[i]!=0)
+        //     {
+        //         nums[ind]=nums[i];
+        //         ind++;
+        //     }
+        // }
+        // for(int i=ind;i<nums.size();i++)
+        // {
+        //     nums[i]=0;
+        // }
+
+        // for reducing loop on adding zeros to end
+        // do this!
+        // optimized code
+        int j=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]!=0)
+            {
+                swap(nums[j],nums[i]);
+                j++;
+            }
+        }
+
+        return nums;
+    }
+};
