9dc040e80dd6c82a6bbe90b17ed8b223d2bf3f68
PRADEEPRAJ <139350539+PRADEEPGITtest@users.noreply.github.com>
2025-03-01 22:45:50 +0530

    Time: 0 ms (100%), Space: 13 MB (87.59%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -25,7 +25,7 @@ public:
 
         // for reducing loop on adding zeros to end
         // do this!
-        // optimized code
+        // optimized code 0(n);
         int j=0;
         for(int i=0;i<nums.size();i++)
         {
