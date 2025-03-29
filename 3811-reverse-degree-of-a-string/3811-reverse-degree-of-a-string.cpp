9ab68f715ea02c8d27b8d57d08a488421d81bab9
PRADEEPRAJ <139350539+PRADEEPGITtest@users.noreply.github.com>
2025-03-29 23:20:47 +0530

    Time: 0 ms (100%), Space: 9.7 MB (80.4%) - LeetHub

---
+++ 3811-reverse-degree-of-a-string/3811-reverse-degree-of-a-string.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        // unordered_map<char,int> mapp={
+        //     {'a',26},
+        //     {'b',25},
+        //     {'c',24},
+        //     {'d',23},
+        //     {'e',22},
+        //     {'f',21},
+        //     {'g',20},
+        //     {'h',19},
+        //     {'i',18},
+        //     {'j',17},
+        //     {'k',16},
+        //     {'l',15},
+        //     {'m',14},
+        //     {'n',13},
+        //     {'o',12},
+        //     {'p',11},
+        //     {'q',10},
+        //     {'r',9},
+        //     {'s',8},
+        //     {'t',7},
+        //     {'u',6},
+        //     {'v',5},
+        //     {'w',4},
+        //     {'x',3},
+        //     {'y',2},
+        //     {'z',1},
+        // };
+
+        // int sum=0, j=1;
+        // for(int i=0;i<s.size();i++)
+        // {
+        //     int rev=(mapp[s[i]])*j;
+        //     cout<<rev<<" ";
+        //     j++;
+        //     sum+=rev;
+        // }
+        // return sum;
+        int sum=0, j=1;
+        for(int i=0;i<s.size();i++)
+        {
+            int rev=(26-(s[i]-'a'))*j;
+            j++;
+            //cout<<rev<<" ";
+            sum+=rev;
+        }
+        return sum;
+    }
+};
