789480df0597c480b8d7cd8a4557c54699bffc0e
PRADEEPRAJ <139350539+PRADEEPGITtest@users.noreply.github.com>
2025-06-02 10:00:37 +0530

    Time: 0 ms (100%), Space: 7.9 MB (18.37%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
-    
-    bool isPowerOfTwo(int n) {
-        //cout<<pow(2,0)<<" ";
-        return fn(0, n);
-    }
-
-    bool fn(int ind, int n){
+bool fn(int ind, int n){
         if( pow(2,ind) == n) return true;
         if( pow(2,ind)> n) return false;
 
         return fn(ind+1, n);
     }
+    
+    bool isPowerOfTwo(int n) {
+        //cout<<pow(2,0)<<" ";
+        //return fn(0, n);
+
+        for(int i=0;i<n;i++){
+            if(pow (2,i) == n) return true;
+            if(pow (2,i)> n) return false;
+        }
+        return false;
+
+    }
+
+    
 };
