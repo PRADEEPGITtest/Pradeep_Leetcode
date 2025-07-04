91b501e0dc9da5780cd921211bde592c17835fd0
PRADEEPRAJ <139350539+PRADEEPGITtest@users.noreply.github.com>
2025-07-04 10:44:41 +0530

    Time: 39 ms (62.55%), Space: 79.1 MB (8.12%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -1,82 +1,30 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        // int n=fruits.size();
-        
-        // int maxlen=0;
-        // for(int i=0;i<n;i++)
-        // {
-        //     set<int> s;
+        int i=0;
+        int j=0;
+        map<int,int> mapp;
+        int maxi=INT_MIN;
 
-        //     for(int j=i;j<n;j++)
-        //     {
-        //         s.insert(fruits[j]);
-
-        //         if(s.size()<=2)
-        //         {
-        //             cout<<j-i<<",";
-        //             maxlen=max(maxlen,j-i+1);
-        //         }
-        //         else break;
-        //     }
-        // }
-        // return maxlen;
-
-        int l=0;
-        int r=0;
-        int maxlen=0;
-        unordered_map<int,int> mapp;
-        while(r<fruits.size())
-        {
-            
-            mapp[fruits[r]]++;
-
-            if(mapp.size()<=2)
-            {
-                maxlen=max(maxlen,r-l+1);
+        while(i<fruits.size() && j<fruits.size()){
+            mapp[fruits[j]]++;
+            if(mapp.size()<=2){
+                maxi=max(maxi,j-i+1);
             }
-            else
-            {
-                while(mapp.size()>2)
-                {
-                    mapp[fruits[l]]--;
-                    if(mapp[fruits[l]] == 0)
-                    {
-                        mapp.erase(fruits[l]);
+            else if(mapp.size()>2){
+                while(mapp.size()>2 ){
+                    mapp[fruits[i]]--;
+                    if(mapp[fruits[i]]==0){
+                        mapp.erase(fruits[i]);
                     }
-                    l++;
+                    i++;
                 }
+                maxi=max(maxi,j-i+1);
             }
-            r++;
+            j++;
         }
-        return maxlen;
 
-        // map<int,int>mpp;
-        // for(int i=0;i<fruits.size();i++)
-        //     mpp[fruits[i]]+=1;
-        // vector<int>nums;
-        // auto it=mpp.begin();
-        // while(it!=mpp.end()){
-        //     nums.push_back(it->second);
-        //     it++;
-        // }
-        // sort(nums.begin(),nums.end(),greater<int>());
-        // int sum=0;
-        // for(int i=0;i<2 && i<nums.size();i++){
-        //     sum+=nums[i];
-        // }
-        // return sum;
-        // int l=0,r=0;
-        // map<int,int>mpp;
-        // int maxi=0;
-        // while(l<fruits.size() && r <fruits.size()){
-        //     mpp[fruits[l]]+=1;
-        //     while(mpp.size()<=2 && r<fruits.size()){
-        //         mpp[fruits[r]]-=1;
-        //         r++;
-        //     }
-        //     maxi=max(maxi,r-l+1);
-        // }
-        // return maxi;
+        return maxi;
+
     }
 };
