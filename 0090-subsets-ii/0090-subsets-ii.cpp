8a7cd404a0ce6af4ee42726b009f8c502d4b55a6
PRADEEPRAJ <139350539+PRADEEPGITtest@users.noreply.github.com>
2025-07-09 23:40:26 +0530

    Time: 0 ms (100%), Space: 10.9 MB (33.31%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,29 +1,55 @@
 class Solution {
 public:
-void solve(vector<int>&nums, vector<int>ds, set<vector<int>>&ans, int ind, int n)
-{
-    if(ind==n){
-        sort(ds.begin(),ds.end());
-        ans.insert(ds);
-        return ;
+// BRUTE FORCE ALGO
+// *************************************************************************
+
+
+//void solve(vector<int>&nums, vector<int>ds, set<vector<int>>&ans, int ind, int n)
+// {
+//     if(ind==n){
+//         sort(ds.begin(),ds.end());
+//         ans.insert(ds);
+//         return ;
+//     }
+
+//     // take:
+//     ds.push_back(nums[ind]);
+//     solve(nums,ds,ans,ind+1,n);
+//     ds.pop_back();
+
+//     // not take:
+//     solve(nums,ds,ans,ind+1,n);
+// }
+
+//*****************************************************************************
+
+void fn(int ind, vector<vector<int>>&ans, vector<int>ds, vector<int>&nums){
+    ans.push_back(ds);
+    for(int i=ind;i<nums.size();i++){
+        if(i!=ind && nums[i]==nums[i-1]) continue;
+        ds.push_back(nums[i]);
+        fn(i+1,ans,ds,nums);
+        ds.pop_back();
     }
-
-    // take:
-    ds.push_back(nums[ind]);
-    solve(nums,ds,ans,ind+1,n);
-    ds.pop_back();
-
-    // not take:
-    solve(nums,ds,ans,ind+1,n);
 }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<int>ds;
-        set<vector<int>> ans;
+        //set<vector<int>> ans;
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
         int n=nums.size();
 
-        solve(nums,ds,ans,0,n);
-        vector<vector<int>> fin(ans.begin(),ans.end());
-        return fin;
+        //solve(nums,ds,ans,0,n);
+        //vector<vector<int>> fin(ans.begin(),ans.end());
+        //return fin;
+
+        // **************
+        // OPTIMAL APROACH
+
+        fn(0,ans,ds,nums);
+        return ans;
+
+
 
     }
 };
