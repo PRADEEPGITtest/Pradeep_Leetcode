3bb7706d9cd7e2217e2bbf15970274a6627ff9da
PRADEEPRAJ <139350539+PRADEEPGITtest@users.noreply.github.com>
2025-07-22 12:34:57 +0530

    Time: 352 ms (5.01%), Space: 105.2 MB (68.18%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
@@ -1,42 +1,23 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        int maxlen=0;
+        map<int,int>mapp;
+        int maxi=INT_MIN;
+        int l=0, r=0;
         int sum=0;
-        // for(int i=0;i<nums.size();i++)
-        // {
-        //     int hash[100000]={0};
-        //     sum=0;
-        //     for(int j=i;j<nums.size();j++)
-        //     {
-        //         if(hash[nums[j]]==1) {
-        //             break;
-        //         }
-
-        //         sum+=nums[j];
-        //         hash[nums[j]]=1;
-        //     }
-        //     maxlen=max(maxlen, sum);
-        // }
-        // return maxlen;
-
-        int l=0,r=0;
-        unordered_map<int,int> mapp;
-        while(r<nums.size())
-        {
+        while(r<nums.size()){
             mapp[nums[r]]++;
             sum=sum+nums[r];
 
-            while(mapp[nums[r]]>1)
-            {
-                sum-=nums[l];
+            while(mapp[nums[r]]>1){
                 mapp[nums[l]]--;
+                if(mapp[nums[l]]==0) mapp.erase(mapp[nums[l]]);
+                sum-=nums[l];
                 l++;
             }
-
-            maxlen=max(maxlen, sum);
+            maxi=max(maxi,sum);
             r++;
         }
-        return maxlen;
+        return maxi;
     }
 };
