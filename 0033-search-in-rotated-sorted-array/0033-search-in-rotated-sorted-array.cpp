83a6f38429e06ce66a9098f572846744d17c4e64
PRADEEPRAJ <139350539+PRADEEPGITtest@users.noreply.github.com>
2025-08-11 16:33:21 +0530

    Time: 0 ms (100%), Space: 15.2 MB (65.86%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,42 +1,29 @@
 class Solution {
 public:
-// // int bs(vector<int>nums, int left, int right, int tar){
-// //     if(left<=right){
-// //         int mid=(left+right)/2;
-// //         if(nums[mid] == tar) return mid;
-// //         else if(tar<nums[mid]) return bs(nums, left, mid-1, tar);
-// //         else return bs(nums, mid+1, right, tar);
-// //     }
-// // }
     int search(vector<int>& nums, int tar) {
-//         int index, flag=0;
-//         for(int i=0;i<nums.size();i++){
-//             if(tar == nums[i]) { flag=1; index=i; }
-//         }
-//         cout<<index<<"\n";
-//         if(flag== 0) return -1;
+        
+        int low=0, high=nums.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==tar) return mid;
+            // left part
+            if(nums[low]<=nums[mid]){
+                if(nums[low]<=tar && tar<=nums[mid]) high=mid-1;
+                else low=mid+1;
+            }
+            else{
+                if(nums[mid]<=tar && tar<=nums[high]) low=mid+1;
+                else high=mid-1;
+            }
+        }
+        return -1;
 
-//         for(int i=nums.size()-1;i>=index;i--){
-//             int temp=nums[i];
-//             nums.pop_back();
-//             nums.insert(nums.begin(),temp);
-//         }
 
-//         for(auto it:nums){
-//             cout<<it<<" ";
-//         }
+        // it accepts for the problem but use binary serach for better learning
 
-//         //int left=0, mid, right=nums.size()-1;
-
-//         //int ans=bs(nums, left, right, tar);
-//         if(is_sorted(nums.begin(), nums.end())){
-//             return index;
-//         }
-//         return -1;
-
-           for(int i=0;i<nums.size();i++){
-            if(tar==nums[i]) return i;
-           }
-           return -1;
+        //    for(int i=0;i<nums.size();i++){
+        //     if(tar==nums[i]) return i;
+        //    }
+        //    return -1;
     }
 };
