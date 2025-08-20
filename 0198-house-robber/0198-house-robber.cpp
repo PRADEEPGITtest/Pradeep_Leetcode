ef7e97d9a155315bd346eef38a9ed93761ce4091
PRADEEPRAJ <139350539+PRADEEPGITtest@users.noreply.github.com>
2025-08-20 11:58:27 +0530

    Time: 0 ms (100%), Space: 10.6 MB (46.66%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
-    int solve(vector<int>&nums, int ind, vector<int>&dp){
-        if(ind==0) return nums[ind];
-        if(ind<0) return 0;
-        if(dp[ind]!=-1) return dp[ind];
+    // int solve(vector<int>&nums, int ind, vector<int>&dp){
+    //     if(ind==0) return nums[ind];
+    //     if(ind<0) return 0;
+    //     if(dp[ind]!=-1) {return dp[ind];}
 
-        int pick=nums[ind]+solve(nums, ind-2, dp);
-        int notpick= 0+solve(nums, ind-1, dp);
+    //     int pick=nums[ind]+solve(nums, ind-2, dp);
+    //     int notpick= 0+solve(nums, ind-1, dp);
 
-        return dp[ind]=max(pick, notpick);
-    }
+    //     int ans=dp[ind]=max(pick, notpick);
+    //     return ans;
+    // }
     int rob(vector<int>& nums) {
-        int n=nums.size()-1;
-        vector<int>dp(n+1,-1);
-        return solve(nums, n, dp);
+        int n=nums.size();
+        //vector<int>dp(n+1,-1);
+
+        //int ans = solve(nums, n, dp);
+        // for(auto i: dp) cout<<i<<" ";
+        // cout<<endl;
+
+        //return ans;
+        vector<int>dp(n);
+        dp[0]=nums[0];
+
+        for(int i=1;i<n;i++){
+            int take=nums[i];
+            if(i>1) {
+                take+=dp[i-2];
+            }
+            int nottake=0+dp[i-1];
+            dp[i]=max(take, nottake);
+        }
+        return dp[n-1];
     }
 };
