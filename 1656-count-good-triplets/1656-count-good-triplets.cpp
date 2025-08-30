385c415b46aae33a699064c12547e486971b87f8
PRADEEPRAJ <139350539+PRADEEPGITtest@users.noreply.github.com>
2025-08-30 16:38:02 +0530

    Time: 8 ms (47.92%), Space: 11.1 MB (40.23%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.cpp
+++ 1656-count-good-triplets/1656-count-good-triplets.cpp
@@ -5,7 +5,10 @@ public:
         for(int i=0;i<arr.size()-2;i++){
             for(int j=i+1;j<arr.size()-1;j++){
                 for(int k=j+1;k<arr.size();k++){
-                    if(abs(arr[i]-arr[j])<=a && abs(arr[j]-arr[k])<=b && abs(arr[i]-arr[k])<=c)
+
+                    if(abs(arr[i]-arr[j])<=a && 
+                    abs(arr[j]-arr[k])<=b && 
+                    abs(arr[i]-arr[k])<=c)
                     {
                         //cout<<arr[i]<<","<<arr[j]<<","<<arr[k];
                         cnt++;
