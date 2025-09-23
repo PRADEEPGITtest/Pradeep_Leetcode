7e65791437a802f4fd52f9a44ebfe1afc9974562
PRADEEPRAJ <139350539+PRADEEPGITtest@users.noreply.github.com>
2025-09-23 21:49:47 +0530

    Time: 0 ms (100%), Space: 11.1 MB (99.73%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -15,7 +15,29 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obsGrid) {
         int n=obsGrid.size();
         int m=obsGrid[0].size();
-        vector<vector<int>>dp(n, vector<int>(m,-1));
-        return fn(n-1, m-1, obsGrid, dp);
+        //vector<vector<int>>dp(n, vector<int>(m,-1));
+        //return fn(n-1, m-1, obsGrid, dp);
+
+        int dp[n][m];
+
+        for(int i=0; i<n; i++){
+            
+            for(int j=0; j<m; j++){
+                
+                if(obsGrid[i][j] == 1) dp[i][j]=0;
+                else if(i==0 && j==0) dp[i][j]=1;
+
+                else{
+
+                    int up=0, left=0;
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+
+                    dp[i][j]=up+left;
+                }
+
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
