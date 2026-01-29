fe2f2ae6c336277730a5ecc6dc97dfaaa50cb3dd
PRADEEP RAJ <139350539+bpradeepraj2005@users.noreply.github.com>
2026-01-29 10:55:42 +0530

    Time: 380 ms (16.89%), Space: 21.3 MB (60.07%) - LeetHub

---
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    int fn(vector<vector<int>>& grid, vector<vector<int>>&dp, int row, int col, int n){
+        if(col>n || col<0 ) return 1e9;
+        if(row==0) return grid[row][col];
+        if(dp[row][col]!=-1) return dp[row][col];
+
+        int ans=1e9;
+        for(int i=0; i<=n; i++){
+            if(col!=i){
+                ans = min(ans, (grid[row][col] + fn(grid, dp, row-1, i, n)));
+            }
+        }
+        return dp[row][col]=ans;
+    }
+public:
+    int minFallingPathSum(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int path_min=1e9;
+        vector<vector<int>>dp(n, vector<int>(n+1, -1));
+        for(int i=0; i<n; i++){
+            path_min=min(path_min, fn(grid, dp, n-1, i, n-1));
+        }
+        return path_min;
+        
+    }
+};
